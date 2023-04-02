4ab1508406c4bd06f1ca1f2c70804b450af733a8
Your Name <zhanna.prokopova.mail@gmail.com>
2023-04-02 17:37:40 +0200

    Initial commit

---
+++ src/main/java/oop/Auto.java
@@ -0,0 +1,25 @@
+package oop;
+
+
+public class Auto {
+    //Instanzen
+    private String KFZKennzeichen;
+    private double Kilometerstand;
+    private double Tankvolumen;
+    private double Kraftstoffverbrauch;
+    private double Kraftstoffmenge;
+
+    //Konstruktor
+    public Auto(KFZKennzeichen, Kilometerstand, Tankvolumen, Kraftstoffverbrauch, Kraftstoffmenge){
+        this.KFZKennzeichen = KFZKennzeichen;
+        this.Kilometerstand = Kilometerstand;
+        this.Tankvolumen = Tankvolumen;
+        this.Kraftstoffverbrauch = Kraftstoffverbrauch;
+        this.Kraftstoffmenge = Kraftstoffmenge;
+    }
+   
+
+    }
+
+
+
---
+++ src/main/java/oop/Main.java
@@ -0,0 +1,8 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto first= new Auto(KFZKennzeichen: "B2222");
+
+    }
+}
