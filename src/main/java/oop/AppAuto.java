f3034aa32bb2efc2bec571e52b7eeadb6a62da58
Your Name <zhanna.prokopova.mail@gmail.com>
2023-04-15 15:45:19 +0200

    AppAuto

---
+++ src/main/java/oop/AppAuto.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class AppAuto {
+    public static void main(String[] args) {
+        Auto NewCar1 = new Auto("B-1111", 10.000, 100, 11.0, 10);
+        Auto NewCar2 = new Auto("B-2222", 20.000, 200, 12.0, 20);
+
+    }
+}
+
+
