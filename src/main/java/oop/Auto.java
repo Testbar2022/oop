bbac55f72fd000630c17738a900255c7adc8bf08
Your Name <zhanna.prokopova.mail@gmail.com>
2023-04-15 15:46:04 +0200

    1

--- src/main/java/oop/Auto.java
+++ src/main/java/oop/Auto.java
@@ -1,5 +1,5 @@
 package oop;
-
+import java.util.Scanner;
 
 public class Auto {
     //Instanzen
@@ -10,16 +10,46 @@ public class Auto {
     private double Kraftstoffmenge;
 
     //Konstruktor
-    public Auto(KFZKennzeichen, Kilometerstand, Tankvolumen, Kraftstoffverbrauch, Kraftstoffmenge){
+    public Auto(String KFZKennzeichen, double Kilometerstand, double Tankvolumen, double Kraftstoffverbrauch, double Kraftstoffmenge){
         this.KFZKennzeichen = KFZKennzeichen;
         this.Kilometerstand = Kilometerstand;
         this.Tankvolumen = Tankvolumen;
         this.Kraftstoffverbrauch = Kraftstoffverbrauch;
         this.Kraftstoffmenge = Kraftstoffmenge;
     }
-   
 
+    public String getKennzeichen() {
+        return KFZKennzeichen;
     }
+
+    public double getKilometerstand() {
+        return Kilometerstand;
+    }
+
+    public double getTankvolumen() {
+        return Tankvolumen;
+    }
+
+
+    public void tanken(double Menge) {                              //Tanken=Menge
+        double neuerKraftstoff = Kraftstoffmenge + Menge;            // tanken mit neuem & bestandstoff
+        if (neuerKraftstoff <= Tankvolumen) {                         // wenn beim tanken das Volumen des Tanks überschritten wird dann bleit maximaler Wert = Tankwollumen
+            Kraftstoffmenge = Tankvolumen;
+        }}
+
+
+        public void fahren(double Strecke) {                                    // Fahren=Strecke
+            double verbrauchterKraftstoff = Strecke / Kraftstoffverbrauch;       // Verbrauch berechnen
+            if (verbrauchterKraftstoff > Kraftstoffmenge) {                         // Verbrauch größer als tatsächliche Menge
+                System.out.println("Reserve, Tank bald leer");
+            } else {
+                Kilometerstand += Strecke;                                             // zum KMStand wird die Strecke gefahrene Strecke dazugefügt
+                Kraftstoffmenge -= verbrauchterKraftstoff;                          // vom aklteullen Kraftstoff wird Verrauch abgezogen
+            }
+        }
+}
+
+
 
 
 
